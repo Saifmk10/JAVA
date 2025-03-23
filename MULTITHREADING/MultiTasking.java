c1be6488fd6a0f372356616c3184af22b6c5d374
Saifmk10 <saifmkpvt@gmail.com>
2025-03-23 22:43:30 +0530

    new module

---
+++ MULTITHREADING/MultiTasking.java
@@ -0,0 +1,60 @@
+import java.util.*;
+import java.io.*;
+
+class MyTasks{
+    public int sum(){
+        int a = 10; 
+        int b = 20; 
+
+        System.out.printf("\nExecuted sum() in : %d's" , System.nanoTime());
+
+        return a+b;
+    }
+
+    public int diff(){
+        int a = 90; 
+        int b = 20; 
+
+        System.out.printf("\nExecuted diff() in : %d's" , System.currentTimeMillis());
+
+        return a-b;
+    }
+}
+
+class MyTasksRunning extends Thread{
+    private MyTasks myTasks = new MyTasks(); 
+    
+    public void run(){
+        
+        int total = myTasks.sum();
+        System.out.printf("\n%d is the ToTaL" , total);
+
+        try{
+            Thread.sleep(1000);
+        }   
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+
+        int differ = myTasks.diff();
+        System.out.printf("\n%d is the DiFfErAnCe" , differ);
+
+    }
+
+}
+
+
+public class MultiTasking{
+    public static void main(String[] argument){
+        MyTasksRunning tasks = new MyTasksRunning();
+
+        tasks.start();
+
+        try{
+            tasks.join();
+        }
+        catch(InterruptedException exception){
+            exception.printStackTrace();
+        }
+    }
+}
